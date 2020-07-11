bfdf70293f78939c414559c83c62511ca45e0d24
Nafiz Ahmed <redoyahmed11@gmail.com>
2020-07-12 03:04:09 +0600

    Incorporate the add on feature on Todo

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import SortableTree from 'react-sortable-tree';
 import './Todo.css';
 import { Paper, Container, Typography, IconButton, Icon } from '@material-ui/core';
+import AddProject from '../AddProject/AddProject';
 
 export interface FlexObj {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -15,33 +16,56 @@ const INITIAL_TODO_ITEMS: FlexObj[] = [
 
 const Todo: React.FC = () => {
   const [todoList, setTodoList] = React.useState<FlexObj[]>(INITIAL_TODO_ITEMS);
+  const [open, setOpen] = React.useState(false);
   const treeOnChangeHandler = (treeData: FlexObj[]) => setTodoList(treeData);
+  const saveHandler = (name: string) => {
+    setTodoList([...todoList, { title: name, children: [] }]);
+    setOpen(false);
+  };
+  const openHandler = () => {
+    setOpen(true);
+  };
+  const closeHandler = () => {
+    setOpen(false);
+  };
   return (
-    <Container className="Todo-container" maxWidth="sm" component={Paper}>
-      <div className="Todo-header-container">
-        <div className="Todo-subtitle">
-          <Typography variant="subtitle1" color="textSecondary">
-            Current List
-          </Typography>
+    <React.Fragment>
+      <AddProject open={open} closeHandler={closeHandler} saveHandler={saveHandler} />
+      <Container className="Todo-container" maxWidth="sm" component={Paper}>
+        <div className="Todo-header-container">
+          <div className="Todo-subtitle">
+            <Typography variant="subtitle1" color="textSecondary">
+              Current List
+            </Typography>
+          </div>
+          <div className="Todo-header">
+            <Typography variant="h6"> Your Projects </Typography>
+          </div>
+          <IconButton className="Todo-add" onClick={openHandler}>
+            <Icon>add</Icon>
+          </IconButton>
         </div>
-        <div className="Todo-header">
-          <Typography variant="h6"> Your Projects </Typography>
+        <div className="Todo-body">
+          <SortableTree
+            treeData={todoList}
+            onChange={treeOnChangeHandler}
+            generateNodeProps={({ node }) => ({
+              title: (
+                <div>
+                  <span style={{ verticalAlign: 'sub', margin: '0px 10px' }}>
+                    <Icon fontSize="small" className="item-dot">
+                      fiber_manual_record
+                    </Icon>
+                  </span>
+                  {node.title}
+                </div>
+              ),
+            })}
+            isVirtualized={false}
+          />
         </div>
-        <IconButton className="Todo-add">
-          <Icon>add</Icon>
-        </IconButton>
-      </div>
-      <div className="Todo-body">
-        <SortableTree
-          treeData={todoList}
-          onChange={treeOnChangeHandler}
-          generateNodeProps={({ node }) => ({
-            title: <div>{node.title}</div>,
-          })}
-          isVirtualized={false}
-        />
-      </div>
-    </Container>
+      </Container>
+    </React.Fragment>
   );
 };
 
