13b86491d06b53a861ad4ce03cd57745199e330e
Nafiz Ahmed <redoyahmed11@gmail.com>
2020-07-12 01:40:43 +0600

    Modify the layout and styles with material ui.

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import SortableTree from 'react-sortable-tree';
 import './Todo.css';
+import { Paper, Container, Typography, IconButton, Icon } from '@material-ui/core';
 
 export interface FlexObj {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -16,16 +17,31 @@ const Todo: React.FC = () => {
   const [todoList, setTodoList] = React.useState<FlexObj[]>(INITIAL_TODO_ITEMS);
   const treeOnChangeHandler = (treeData: FlexObj[]) => setTodoList(treeData);
   return (
-    <div>
-      <SortableTree
-        treeData={todoList}
-        onChange={treeOnChangeHandler}
-        generateNodeProps={({ node }) => ({
-          title: <div>{node.title}</div>,
-        })}
-        isVirtualized={false}
-      />
-    </div>
+    <Container className="Todo-container" maxWidth="sm" component={Paper}>
+      <div className="Todo-header-container">
+        <div className="Todo-subtitle">
+          <Typography variant="subtitle1" color="textSecondary">
+            Current List
+          </Typography>
+        </div>
+        <div className="Todo-header">
+          <Typography variant="h6"> Your Projects </Typography>
+        </div>
+        <IconButton className="Todo-add">
+          <Icon>add</Icon>
+        </IconButton>
+      </div>
+      <div className="Todo-body">
+        <SortableTree
+          treeData={todoList}
+          onChange={treeOnChangeHandler}
+          generateNodeProps={({ node }) => ({
+            title: <div>{node.title}</div>,
+          })}
+          isVirtualized={false}
+        />
+      </div>
+    </Container>
   );
 };
 
