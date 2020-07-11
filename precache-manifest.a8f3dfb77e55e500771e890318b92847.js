3e492cfbca20223f44a6c08a1c862362ca7ca68c
Nafiz Ahmed <redoyahmed11@gmail.com>
2020-07-12 04:46:59 +0600

    Updates

---
+++ precache-manifest.a8f3dfb77e55e500771e890318b92847.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c196755e1cd0c944ed2251117e7bf9fa",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "7a4a7d7c95f0d746df95",
+    "url": "/todo-list/static/css/2.074064f9.chunk.css"
+  },
+  {
+    "revision": "a5bb4922076a9e48bb82",
+    "url": "/todo-list/static/css/main.de2da6c1.chunk.css"
+  },
+  {
+    "revision": "7a4a7d7c95f0d746df95",
+    "url": "/todo-list/static/js/2.a2b1944c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo-list/static/js/2.a2b1944c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5bb4922076a9e48bb82",
+    "url": "/todo-list/static/js/main.47aaf0f3.chunk.js"
+  },
+  {
+    "revision": "4c207b602d40809001a5",
+    "url": "/todo-list/static/js/runtime-main.2929926a.js"
+  }
+]);
